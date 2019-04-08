175bb271a3602a20288627fad316c68df5474c47
Asunqingwen <sqw123az@sina.com>
2019-04-08 18:03:45 +0800

    update codes of chapter4

---
+++ chapter4/qt04_lineEdit02.py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QFormLayout
+from PyQt5.QtGui import QIntValidator, QDoubleValidator, QRegExpValidator
+from PyQt5.QtCore import QRegExp
+import sys
+
+
+class lineEditDemo(QWidget):
+	def __init__(self, parent=None):
+		super(lineEditDemo, self).__init__(parent)
+		self.setWindowTitle('QLineEdit例子')
+
+		flo = QFormLayout()
+		pIntLineEdit = QLineEdit()
+		pDoubleLineEdit = QLineEdit()
+		pValidatorLineEdit = QLineEdit()
+
+		flo.addRow('整型', pIntLineEdit)
+		flo.addRow('浮点型', pDoubleLineEdit)
+		flo.addRow('字母和数字', pValidatorLineEdit)
+
+		pIntLineEdit.setPlaceholderText('整型')
+		pDoubleLineEdit.setPlaceholderText('浮点型')
+		pValidatorLineEdit.setPlaceholderText('字母和数字')
+
+		# 整型，范围[1,99]
+		pIntValidator = QIntValidator(self)
+		pIntValidator.setRange(1, 99)
+
+		# 浮点型，范围[-360,360]，精度：小数点后两位
+		pDoubleValidator = QDoubleValidator(self)
+		pDoubleValidator.setRange(-360, 360)
+		pDoubleValidator.setNotation(QDoubleValidator.StandardNotation)
+		pDoubleValidator.setDecimals(2)
+
+		# 字母和数字
+		reg = QRegExp('[a-zA-Z0-9]+$')
+		pValidator = QRegExpValidator(self)
+		pValidator.setRegExp(reg)
+
+		# 设置验证器
+		pIntLineEdit.setValidator(pIntValidator)
+		pDoubleLineEdit.setValidator(pDoubleValidator)
+		pValidatorLineEdit.setValidator(pValidator)
+
+		self.setLayout(flo)
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	win = lineEditDemo()
+	win.show()
+	sys.exit(app.exec())
